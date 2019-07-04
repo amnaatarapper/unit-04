ad674face4c78d5063bf3ee3e060006592717805
amnaatarapper <44092179+amnaatarapper@users.noreply.github.com>
2019-07-04 12:00:43 +0100

    addPhraseToDisplay()

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game{
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrase();
+        this.activePhrase = null;
+    }
+
+    createPhrase() { 
+        const phrases = [
+            {phrase: 'this is so cool'},
+            {phrase: 'this is so cool'},
+            {phrase: 'this is so cool'},
+            {phrase: 'this is so cool'},
+            {phrase: 'this is so cool'}];
+
+            return phrases
+    }
+
+    getRandomPhrase() {
+
+        const index = Math.floor(Math.random() * this.phrases.length);
+        console.log(index)
+        console.log(this.phrases[index])
+
+        
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase{
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    
+    addPhraseToDisplay() {
+
+        const ul = document.querySelector('ul');
+    
+        const letter = this.phrase.split("");
+
+        for ( let letter of this.phrase) {
+
+            const li = document.createElement('li');
+            li.classList.add('hide', 'letter', letter);
+            ul.appendChild(li)
+
+        }
+
+
+
+        
+
+    }
+}
